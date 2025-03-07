9433508cf3f8f78ed7e98d2dc56a2247933c9ea4
viktorradu <v.radu@outlook.com>
2025-03-07 09:23:54 -0500

    Batch logging and empty collections fix

--- inventory.py
+++ inventory.py
@@ -13,7 +13,7 @@ workspaces = scan.list_workspaces()
 if 'error' not in workspaces:
     batch_size = 100
     batch_count = ceil(len(workspaces) / batch_size)
-    for batch in range(0, len(workspaces), batch_size):
+    for batch in range(0, batch_count):
         batch_workspaces = workspaces[batch:batch + batch_size]
         workspace_ids = [workspace['id'] for workspace in batch_workspaces]
         print(f"Scaning workspace batch {batch + 1} out of {batch_count}")
@@ -21,12 +21,13 @@ if 'error' not in workspaces:
         print(f"Scan completed for {len(workspace_ids)} workspaces.")
         
         for collection in scan_result.keys():      
-            file_path = f'{path}/{collection}.csv'
-            is_new = not os.path.exists(file_path)
-            with open(file_path, 'a', newline='', encoding="utf-8") as file:
-                writer = csv.DictWriter(file,fieldnames=scan_result[collection][0].keys(),extrasaction='ignore', quoting=csv.QUOTE_ALL, lineterminator='\n')
-                if is_new:
-                    writer.writeheader()
-                writer.writerows(scan_result[collection])
+            if len(scan_result[collection]) > 0:
+                file_path = f'{path}/{collection}.csv'
+                is_new = not os.path.exists(file_path)
+                with open(file_path, 'a', newline='', encoding="utf-8") as file:
+                    writer = csv.DictWriter(file,fieldnames=scan_result[collection][0].keys(),extrasaction='ignore', quoting=csv.QUOTE_ALL, lineterminator='\n')
+                    if is_new:
+                        writer.writeheader()
+                    writer.writerows(scan_result[collection])
 else:
     print(workspaces.error)
